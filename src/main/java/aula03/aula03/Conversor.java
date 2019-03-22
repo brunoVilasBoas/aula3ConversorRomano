5c2551bdd8a3b97cf5be1d71df147c3759dc19df
Bruno Vilas Boas <brunoordoni@gmail.com>
2019-03-21 21:13:10 -0300

    Iniciando Implementação

--- src/main/java/aula03/aula03/Conversor.java
+++ src/main/java/aula03/aula03/Conversor.java
@@ -1,11 +1,31 @@
 package aula03.aula03;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Conversor {
 
 	public static void main(String[] args) {
-		  
-		System.out.println( "Hello World!" );
-
+		
+		
+		Scanner sc = new Scanner(System.in); 
+		Integer numero = null;
+		
+		while(true) {
+			try {
+				System.out.print("Digite um número de (1 a 3000): ");
+		        // String input 
+				numero = sc.nextInt();
+				break;
+				
+			} catch (InputMismatchException e) {
+				
+				System.out.println("Erro! Digite um número!");
+			}
+		}
+    
+        
+        System.out.print(numero); 
 	}
 
 }
